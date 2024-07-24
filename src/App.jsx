f8aa74b5bec4947ce6fb8f0ac36626da3fa4e0eb
1Matzh <matheusbenasse@gmail.com>
2024-07-23 21:54:19 -0300

    Small correction

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Education from './components/Education';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Certificates from './components/Certificates';
-import Language from './components/Language';
 
 const App = () => (
   <div className="container mt-5">
@@ -21,9 +20,6 @@ const App = () => (
       </div>
       <div className="col-12 mb-3 p-5 rounded bg">
         <Certificates />
-      </div>
-      <div className="col-12 mb-3 p-5 rounded bg">
-        <Language />
       </div>
     </div>
   </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Telephone, GeoAlt, Envelope, Linkedin } from 'react-bootstrap-icons';
+import { Telephone, GeoAlt, Envelope, Linkedin, Github } from 'react-bootstrap-icons';
 
 const Contact = () => (
   <div className="mb-4">
@@ -8,6 +8,7 @@ const Contact = () => (
       <Telephone size={18} /> (11) 97679-0731<br />
       <Envelope size={18} /> matheusbenasse@gmail.com<br />
       <Linkedin size={18} /> linkedin.com/in/matheusgrecchibenasse/<br />
+      <Github size={18} /> github.com/1Matzh<br />
       <GeoAlt size={18} /> Taboão da Serra, SP
     </p>
   </div>
